f70ca4e2e9abbb3d7aa65f8ac92bc1bffe6ca3e4
John Gardner <huxoll@gmail.com>
2025-10-02 23:00:26 -0500

    Remove old static site.

--- themes/paper/.prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-
-  // https://github.com/tailwindlabs/prettier-plugin-tailwindcss
-  plugins: [require('prettier-plugin-tailwindcss')],
-};
